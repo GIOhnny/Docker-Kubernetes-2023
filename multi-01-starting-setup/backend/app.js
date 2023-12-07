8fb075daff847b6c635575325bd1ebfe4d383d83
GIOhnny <giohnny@gmail.com>
2023-12-07 22:45:02 +0200

    Sec 5 - ENV vars + dockerignore

--- multi-01-starting-setup/backend/app.js
+++ multi-01-starting-setup/backend/app.js
@@ -85,7 +85,7 @@ app.delete('/goals/:id', async (req, res) => {
 
 //'mongodb://localhost:27017/course-goals',
 mongoose.connect(
-  'mongodb://GIOhnny:secret@mongodb:27017/course-goals?authSource=admin',
+  `mongodb://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@mongodb:27017/course-goals?authSource=admin`,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
